348585fc42739d548ec7f9c6fb689e9b16476850
Dinesh Tyagi <dinesh777tyagi@gmail.com>
2020-10-19 16:37:46 +0530

    Updates

---
+++ precache-manifest.216a7e8565ff6c913773b78a5ccd5817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b443cd808ea5d8d76d40dddca0891674",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "a1ccef0aaeae04848d29",
+    "url": "/form/static/css/main.a748a8d0.chunk.css"
+  },
+  {
+    "revision": "06fa8d7179b7fff644d3",
+    "url": "/form/static/js/2.4b53ac7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form/static/js/2.4b53ac7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1ccef0aaeae04848d29",
+    "url": "/form/static/js/main.06f47d40.chunk.js"
+  },
+  {
+    "revision": "9cc23689b2e8581e1271",
+    "url": "/form/static/js/runtime-main.01d241c6.js"
+  }
+]);
